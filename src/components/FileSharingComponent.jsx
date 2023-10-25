aa35a81994024b3c41a2bb1bbe7fea9995b39ede
ek-sarath <sarath.e@edstem.com>
2023-10-26 01:48:46 +0530

    Commit

--- src/components/FileSharingComponent.jsx
+++ src/components/FileSharingComponent.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import '../App.css';
 import Group19 from '../assets/Group19.svg';
+import LearnMoreIcon from '../assets/LearnMoreIcon.svg';
 
 function FileSharingComponent(props) {
   return (
     <div className="Centre">
+
+     <div className="LeftSocial">
       <h1>
         Share text, voice,<br/> photos, videos and <br/> GIFs files for free
-      </h1>
-      <p>Feugiat primis a ligula undo auctor mauris auctor <br/> laoreet and pretium augue an egestas cubilia</p>
-      <button>Learn More</button>
+         </h1>
+          <p>Feugiat primis a ligula undo auctor mauris auctor <br/> laoreet and pretium augue an egestas cubilia</p>
+          <button class="LearnMoreButton">Learn More</button>
+          <img src={LearnMoreIcon} alt="Laern More"/>
+          See OLMO in Action
+      </div>
 
-         <div>
-          <img src={Group19} alt="Group 19" style={{ top: -10, left: 100, width: '30%', height: '30%' }} />
+         <div className="SocialTree">
+          <img src={Group19} alt="Group 19"/>
          </div>
     </div>
   );

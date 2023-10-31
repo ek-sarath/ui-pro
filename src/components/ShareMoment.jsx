9cec826acfd67d8ca98f58e6aeac0a75526f6b6f
ek-sarath <sarath.e@edstem.com>
2023-11-01 00:55:15 +0530

    All Done

--- src/components/ShareMoment.jsx
+++ src/components/ShareMoment.jsx
@@ -6,15 +6,19 @@ import LapMan from '../assets/LaptopMan.svg';
 
 const ShareMoment = () => {
     return (
+
+        
         <div className='SecurityHeading'>
            <h3>Share Moments. Share Life.</h3>
            <h6>Aliquam a augue suscipit, luctus neque purus ipsum neque dolor primis a<br/>libero tempus, blandit and cursus varius and magnis sapien</h6>
            
-           <div className="OlmosTabs">
+        <div className="ColumnTab">
            <img src={Specs} alt='Specs Man' className="Man"/>
            <img src={LapMan} alt='Play Button Man' className="Man"/>
+           <div className="OlmosTabs">
            <button className="OlmoButton">See OLMO in Action</button><br/>
             <br/>30 days free trial <span className="RedPipe">|</span>Exclusive Support<span className="RedPipe">|</span>No Fee
+            </div>
          </div>
         </div>
     );

c4a734cbe59ccadfc5e2fc992d2fa16802f7fcb8
ek-sarath <sarath.e@edstem.com>
2023-10-31 23:35:55 +0530

    Final but Commit

--- src/components/AnnouncementCard.jsx
+++ src/components/AnnouncementCard.jsx
@@ -11,8 +11,7 @@ const AnnouncementCard = () => {
             <div className="right-half">
                 <h1>Share files, media, and locations</h1>
                 <p>
-                    Quaerat sodales sapien euismod blandit purus a purus ipsum primis in cubilia laoreet augue luctus magna dolor luctus and egestas sapien egestas vitae nemo volute</p><br/>
-                   <p> Quaerat sodales sapien euismod blandit at vitae ipsum primis undo and cubilia laoreet augue and luctus magna dolor luctus at egestas sapien vitae nemo egestas volute and turpis dolores aliquam quaerat sodales a sapien
+                Quaerat sodales sapien euismod blandit purus a purus ipsum primis in cubilia laoreet augue luctus magna dolor luctus and egestas sapien egestas vitae nemo volute<br/><br/>Quaerat sodales sapien euismod blandit at vitae ipsum primis undo and cubilia laoreet augue and luctus magna dolor luctus at egestas sapien vitae nemo egestas volute and turpis dolores aliquam quaerat sodales a sapien
                 </p>
             </div>
         </div>
--- src/components/VerificationCard.jsx
+++ src/components/VerificationCard.jsx
@@ -7,18 +7,18 @@ const VerificationCard = () => {
     return (
         <div className='AnnouncementCard'>
             <div className="left-half">
-                <img src={Verification} alt='Verification Card' />
+                <img src={Verification} alt='Verification Card' className="VerifiactionClass" />
             </div>
             <div className="right-half">
                 <h2>Chat with your friends easily</h2>
                 <p>
                 Quaerat sodales sapien euismod blandit at vitae ipsum primis undo and cubilia laoreet augue and luctus magna dolor luctus at egestas sapien vitae nemo egestas volute and turpis dolores aliquam quaerat sodales a sapien
-                </p> 
-                <h2>Chat with your friends easily</h2>
-                <p>
-                Fringilla risus, luctus mauris auctor euismod an iaculis luctus magna purus pretium ligula purus and quaerat
-                Nemo ipsam egestas volute turpis dolores undo ultrice aliquam quaerat at sodales sapien purus
                 </p> <br/>
+
+                <h2>Advanced performance made easy</h2>
+                <p>Fringilla risus, luctus mauris auctor euismod an iaculis luctus magna purus pretium ligula purus and quaerat
+                <br/>Nemo ipsam egestas volute turpis dolores undo ultrice aliquam quaerat at sodales sapien purus
+                </p><br/>
                 <img src={Play} alt='Play Store'/>
             </div>
         </div>

53e8d1feb9b738952ca28a89461954a14a5731f8
ek-sarath <sarath.e@edstem.com>
2023-10-31 17:23:55 +0530

    Almost Done Again

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "../App.css";
+import PlayM from '../assets/playbuttonMan.svg';
 
 const Features = () => {
     return (
         <div className='SecurityHeading'>
            <h3>Discover powerful features to boost your productivity</h3>
+           <img src={PlayM} alt='Play Button Man' className="PlaybuttonMan"/>
         </div>
     );
 };
--- src/components/ShareMoment.jsx
+++ src/components/ShareMoment.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import "../App.css";
+import Specs from '../assets/SpecsMan.svg';
+import LapMan from '../assets/LaptopMan.svg';
+
 
 const ShareMoment = () => {
     return (
         <div className='SecurityHeading'>
            <h3>Share Moments. Share Life.</h3>
-           <h6>Aliquam a augue suscipit, luctus neque purus ipsum neque dolor primis a<br/><br/>libero tempus, blandit and cursus varius and magnis sapien</h6>
-        
-        
+           <h6>Aliquam a augue suscipit, luctus neque purus ipsum neque dolor primis a<br/>libero tempus, blandit and cursus varius and magnis sapien</h6>
+           <img src={Specs} alt='Specs Man' className="Man"/>
+           <img src={LapMan} alt='Play Button Man' className="Man"/>
+
+
+               <div className="OlmosTabs">
+                 <button className="OlmoButton">See OLMO in Action</button><br/>
+               <br/>30 days free trial <span className="RedPipe">|</span>Exclusive Support<span className="RedPipe">|</span>No Fee
+              </div>
+
         </div>
     );
 };

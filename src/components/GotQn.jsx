e3549c222eed721482319cc19d6f0ded27f2ae42
ek-sarath <sarath.e@edstem.com>
2023-11-01 13:27:44 +0530

    Point edges of Got Qns Fixed

--- src/components/GotQn.jsx
+++ src/components/GotQn.jsx
@@ -4,7 +4,7 @@ import "../App.css";
 const GotQn = () => {
     return (
         <div className='SecurityHeading'>
-            <h3>Got Questions? Look Here</h3>
+            <h4>Got Questions? Look Here</h4>
             <h6>Aliquam a augue suscipit, luctus neque purus ipsum neque dolor primis a libero tempus, blandit and cursus varius and magnis sapien</h6>
         </div>
     );
